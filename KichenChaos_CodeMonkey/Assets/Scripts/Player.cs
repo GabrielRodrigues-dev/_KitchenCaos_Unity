e1c841dff3f01ab0e8b0ad38e7ab2da359203fc9
gabriel henrique rodrigues ribeiro costa <gabrielup14@gmail.com>
2024-03-04 23:01:07 -0300

    Basic_MovimentPlayer

--- KichenChaos_CodeMonkey/Assets/Scripts/Player.cs
+++ KichenChaos_CodeMonkey/Assets/Scripts/Player.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float moveSpeed = 7.0f;
+
+    private void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float rotationSpeed = 10.0f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
