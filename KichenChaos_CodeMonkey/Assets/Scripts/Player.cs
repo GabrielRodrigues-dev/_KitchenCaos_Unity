c7f6e45529f5a6e0580dd8b199166151f8b15814
gabriel henrique rodrigues ribeiro costa <gabrielup14@gmail.com>
2024-03-13 21:47:31 -0300

    BasicMove_SetAnimation_CameraAndNewImputSystem

--- KichenChaos_CodeMonkey/Assets/Scripts/Player.cs
+++ KichenChaos_CodeMonkey/Assets/Scripts/Player.cs
@@ -5,38 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] private GameInput gameInput; 
+
     [SerializeField] private float moveSpeed = 7.0f;
+
+
+    private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+       
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotationSpeed = 10.0f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
 
+    }
+
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 
 
